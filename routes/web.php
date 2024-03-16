7b3a108c97fed2750cc166b551c7e222be932c0d
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-03-17 05:57:51 +0700

    register product categories route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +25,6 @@ Route::middleware(['auth'])->group(function () {
     })->name('home');
 
     Route::resource('users', UserController::class);
+
+    Route::resource('product_categories', ProductCategoryController::class);
 });
