5dfd5392892258a70352378374f0b35a6eae84ce
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-03-17 05:58:11 +0700

    add product categories menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -16,6 +16,10 @@
                     <a href="{{ route('users.index') }}"
                     class="nav-link"><i class="fas fa-user"></i><span>Users</span></a>
                 </li>
+                <li class="{{ Request::is('product_categories') ? 'active' : '' }}">
+                    <a href="{{ route('product_categories.index') }}"
+                    class="nav-link"><i class="fas fa-user"></i><span>Product Categories</span></a>
+                </li>
         </ul>
     </aside>
 </div>
